82e3650b5a5bdd367588a7e9dc6ff338bc23f325
Cuong <72295193+Cuong15061999@users.noreply.github.com>
2022-12-27 15:47:03 +0700

    Table data

---
+++ FE ADMIN/admin-app/src/Data/MainDashTableData/Data.js
@@ -0,0 +1,12 @@
+//side bar data
+export const MainDashTableData = [
+    { id: 1, picture: "https://assets.materialup.com/uploads/bebad102-7f40-4941-99cd-54366113003e/avatar-08.png", title: 'Jon', url: 35 , dateTime: '11/12/2022'},
+    { id: 2, picture: 'Lanqater', title: 'Cersei', url: 42 , dateTime: '12/12/2022'},
+    { id: 3, picture: 'Lannister', title: 'Jaime', url: 45 , dateTime: '11/12/2022'},
+    { id: 4, picture: 'Stark', title: 'Arya', url: 16 , dateTime: '11/12/2022'},
+    { id: 5, picture: 'Targaryen', title: 'Daenerys', url: 113 , dateTime: '11/12/2022'},
+    { id: 6, picture: 'Melisandre', title: 'Cuong', url: 150 , dateTime: '11/12/2022'},
+    { id: 7, picture: 'Clifford', title: 'Ferrara', url: 44 , dateTime: '11/12/2022'},
+    { id: 8, picture: 'Frances', title: 'Rossini', url: 36 , dateTime: '11/12/2022'},
+    { id: 9, picture: 'Roxie', title: 'Harvey', url: 65 , dateTime: '11/12/2022'},
+]
--- FE ADMIN/admin-app/src/components/Table/Table.jsx
+++ FE ADMIN/admin-app/src/components/Table/Table.jsx
@@ -3,6 +3,7 @@ import './Table.css'
 import { DataGrid } from '@mui/x-data-grid';
 import Avatar from "@mui/material/Avatar";
 import Button from '@mui/material/Button';
+import { MainDashTableData } from '../../Data/MainDashTableData/Data';
 const columns = [
   { 
     field: 'picture', 
@@ -46,17 +47,7 @@ const columns = [
   },
 ];
 
-const rows = [
-  { id: 1, picture: "https://assets.materialup.com/uploads/bebad102-7f40-4941-99cd-54366113003e/avatar-08.png", title: 'Jon', url: 35 , dateTime: '11/12/2022'},
-  { id: 2, picture: 'Lanqater', title: 'Cersei', url: 42 , dateTime: '12/12/2022'},
-  { id: 3, picture: 'Lannister', title: 'Jaime', url: 45 , dateTime: '11/12/2022'},
-  { id: 4, picture: 'Stark', title: 'Arya', url: 16 , dateTime: '11/12/2022'},
-  { id: 5, picture: 'Targaryen', title: 'Daenerys', url: 113 , dateTime: '11/12/2022'},
-  { id: 6, picture: 'Melisandre', title: 'Cuong', url: 150 , dateTime: '11/12/2022'},
-  { id: 7, picture: 'Clifford', title: 'Ferrara', url: 44 , dateTime: '11/12/2022'},
-  { id: 8, picture: 'Frances', title: 'Rossini', url: 36 , dateTime: '11/12/2022'},
-  { id: 9, picture: 'Roxie', title: 'Harvey', url: 65 , dateTime: '11/12/2022'},
-];
+const rows = MainDashTableData;
 
 export const Table = () => {
   return (
